47842f7e98e04f3bfa106327dc64a33a8302acff
sayan2407 <sayanp166@gmail.com>
2024-03-27 00:43:42 +0530

    Nav Bar add

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav className="say-nav">
+        <div>
+            <h3 id="logo"> Expense Tracker </h3>
+        </div>
+
+    </nav>
+  )
+}
+
+export default NavBar
